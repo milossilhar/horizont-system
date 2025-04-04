8adf65b322a332484f6855e36f6e474e99eb7a0f
milossilhar <migmig095@gmail.com>
2025-04-04 14:32:17 +0200

    new: web security config

---
+++ src/main/java/sk/leziemevpezinku/spring/WebSecurityConfiguration.java
@@ -0,0 +1,23 @@
+package sk.leziemevpezinku.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(
+                        matcher -> matcher.anyRequest().permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
