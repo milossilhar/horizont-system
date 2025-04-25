4260e30aff499f1aca5e39e32195c96f52bf0741
milossilhar <migmig095@gmail.com>
2025-04-25 10:35:46 +0200

    new: updated timezone in PostgreSQL and Spring APP

--- src/main/java/sk/leziemevpezinku/spring/RegistrationSystemApplication.java
+++ src/main/java/sk/leziemevpezinku/spring/RegistrationSystemApplication.java
@@ -1,13 +1,29 @@
 package sk.leziemevpezinku.spring;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableTransactionManagement
 public class RegistrationSystemApplication {
 
+    @Value("${application.timezone:UTC}")
+    private String applicationTimeZone;
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone(applicationTimeZone));
+    }
+
+    /**
+     * Main function that runs Spring Application.
+     * @param args arguments
+     */
     public static void main(String[] args) {
         SpringApplication.run(RegistrationSystemApplication.class, args);
     }
