d54a4d5503a9f3b3f952c95955896e3a9fb1c1fb
milossilhar <migmig095@gmail.com>
2025-04-26 10:14:49 +0200

    chg: removed timezone settings

--- src/main/java/sk/leziemevpezinku/spring/RegistrationSystemApplication.java
+++ src/main/java/sk/leziemevpezinku/spring/RegistrationSystemApplication.java
@@ -1,24 +1,12 @@
 package sk.leziemevpezinku.spring;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import java.util.TimeZone;
-
 @SpringBootApplication
 @EnableTransactionManagement
 public class RegistrationSystemApplication {
-
-    @Value("${application.timezone:UTC}")
-    private String applicationTimeZone;
-
-    @PostConstruct
-    public void init() {
-        TimeZone.setDefault(TimeZone.getTimeZone(applicationTimeZone));
-    }
 
     /**
      * Main function that runs Spring Application.
