14c2930578466cc57f8338f04d473e6bc252647f
milossilhar <migmig095@gmail.com>
2025-06-20 14:23:02 +0200

    rm: removed liquibase updates from BETA run

--- src/main/resources/db/changelog/updates/004-email-flags-registration.sql
+++
@@ -1,6 +0,0 @@
---liquibase formatted sql
---changeset dugong:004_EMAIL_FLAGS_REGISTRATION
-
-ALTER TABLE reg_registration ADD email_confirm_sent boolean;
-ALTER TABLE reg_registration ADD email_payment_confirm_sent boolean;
-ALTER TABLE reg_registration ADD email_payment_info_sent boolean;
