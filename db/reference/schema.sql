1daa6df463ca445069a7a37b0d7228420a297c71
milossilhar <migmig095@gmail.com>
2025-06-20 14:27:21 +0200

    chg: updated schema.sql reference

--- db/reference/schema.sql
+++ db/reference/schema.sql
@@ -7,14 +7,46 @@ create sequence seq_event_id start with 1 increment by 1;
 
 create sequence seq_event_term_id start with 1 increment by 1;
 
+create sequence seq_lesson_id start with 1 increment by 1;
+
 create sequence seq_payment_id start with 1 increment by 1;
 
+create sequence seq_period_id start with 1 increment by 1;
+
+create sequence seq_person_id start with 1 increment by 1;
+
+create sequence seq_place_id start with 1 increment by 1;
+
 create sequence seq_registration_id start with 1 increment by 1;
+
+create sequence seq_user_id start with 1 increment by 1;
 
 create table reg_app_param (
     name varchar(100) not null,
     value varchar(200) not null,
     primary key (name)
+);
+
+create table reg_attendance (
+    created_at timestamp(6) not null,
+    created_by varchar(50),
+    modified_at timestamp(6),
+    modified_by varchar(50),
+    status varchar(10) not null,
+    lesson_id bigint not null,
+    person_id bigint not null,
+    primary key (lesson_id, person_id)
+);
+
+create table reg_attendance_trainer (
+    created_at timestamp(6) not null,
+    created_by varchar(50),
+    modified_at timestamp(6),
+    modified_by varchar(50),
+    duration_minutes integer not null,
+    lesson_id bigint not null,
+    person_id bigint not null,
+    primary key (lesson_id, person_id)
 );
 
 create table reg_email_logs (
@@ -31,7 +63,7 @@ create table reg_enumeration_item (
     id bigint not null,
     code varchar(10) not null,
     description varchar(150) not null,
-    enum_name varchar(40) not null check (enum_name in ('REG_E_EVENT_CONDITION_TYPE','REG_E_EVENT_DISCOUNT_TYPE','REG_E_RELATION','REG_E_SHIRT_SIZE')),
+    enum_name varchar(40) not null,
     ordering integer not null,
     visible boolean not null,
     primary key (id)
@@ -40,25 +72,49 @@ create table reg_enumeration_item (
 create table reg_event (
     id bigint not null,
     created_at timestamp(6) not null,
+    created_by varchar(50),
+    modified_at timestamp(6),
+    modified_by varchar(50),
     uuid varchar(40) not null unique,
     details varchar(2000) not null,
-    discount_type varchar(40),
-    event_type varchar(20) not null check (event_type in ('EVENT','CAMP','SCHOOL_CLIMB','ECA')),
+    event_type varchar(20) not null,
     image_url varchar(100),
+    locked boolean,
     name varchar(200) not null,
-    place varchar(300) not null,
     reg_end_at timestamp(6) not null,
     reg_start_at timestamp(6) not null,
+    period_id bigint not null,
+    place_id bigint not null,
     primary key (id)
+);
+
+create table reg_event_condition (
+    event_id bigint not null,
+    condition_type varchar(20) not null,
+    max_value varchar(50),
+    min_value varchar(50),
+    value varchar(50),
+    ind integer not null,
+    primary key (event_id, ind)
 );
 
 create table reg_event_term (
     id bigint not null,
-    capacity integer not null,
-    deposit numeric(38,2) not null,
-    end_at timestamp(6) not null,
+    created_at timestamp(6) not null,
+    created_by varchar(50),
+    modified_at timestamp(6),
+    modified_by varchar(50),
+    capacity bigint,
+    day_of_week smallint check (day_of_week between 0 and 6),
+    deposit numeric(38,2),
+    duration_minutes integer not null,
+    end_date date,
+    expected_trainers jsonb,
+    has_attendance boolean,
+    number_of_lessons bigint,
     price numeric(38,2) not null,
-    start_at timestamp(6) not null,
+    start_date date not null,
+    start_time time(6) not null,
     event_id bigint not null,
     primary key (id)
 );
@@ -72,6 +128,20 @@ create table reg_known_person (
     primary key (registration_id, ind)
 );
 
+create table reg_lesson (
+    id bigint not null,
+    created_at timestamp(6) not null,
+    created_by varchar(50),
+    modified_at timestamp(6),
+    modified_by varchar(50),
+    duration_minutes integer not null,
+    expected_trainers jsonb,
+    start_at timestamp(6) not null,
+    event_term_id bigint not null,
+    place_id bigint not null,
+    primary key (id)
+);
+
 create table reg_payment (
     id bigint not null,
     deposit numeric(10,2) not null,
@@ -83,8 +153,20 @@ create table reg_payment (
     primary key (id)
 );
 
+create table reg_period (
+    id bigint not null,
+    created_at timestamp(6) not null,
+    created_by varchar(50),
+    modified_at timestamp(6),
+    modified_by varchar(50),
+    end_at date,
+    name varchar(100) not null,
+    start_at date,
+    primary key (id)
+);
+
 create table reg_person (
-    registration_id bigint not null,
+    id bigint not null,
     date_of_birth date not null,
     food_allergy_notes varchar(1000),
     health_notes varchar(1000),
@@ -92,28 +174,113 @@ create table reg_person (
     name varchar(50) not null,
     shirt_size varchar(10),
     surname varchar(50) not null,
-    ind integer not null,
-    primary key (registration_id, ind)
+    user_id bigint,
+    primary key (id)
+);
+
+create table reg_place (
+    id bigint not null,
+    created_at timestamp(6) not null,
+    created_by varchar(50),
+    modified_at timestamp(6),
+    modified_by varchar(50),
+    description varchar(500),
+    latitude numeric(12,8) not null,
+    longitude numeric(12,8) not null,
+    name varchar(100) not null,
+    training_price numeric(38,2),
+    primary key (id)
 );
 
 create table reg_registration (
     id bigint not null,
     created_at timestamp(6) not null,
-    uuid varchar(40) not null unique,
     consent_gdpr boolean not null,
     consent_photo boolean,
     email varchar(100) not null,
-    email_confirm_sent boolean,
-    email_payment_confirm_sent boolean,
-    email_payment_info_sent boolean,
     name varchar(50) not null,
-    status varchar(10) not null check (status in ('CONCEPT','QUEUE','ACCEPTED','CONFIRMED')),
     surname varchar(50) not null,
+    payment_scheme varchar(10),
+    status varchar(10) not null,
     tel_phone varchar(20) not null,
-    event_term_id bigint,
-    payment_id bigint unique,
+    uuid varchar(40) not null,
+    event_term_id bigint not null,
+    person_id bigint not null,
     primary key (id)
 );
+
+create table reg_substitute_lesson (
+    person_id bigint not null,
+    expires_at timestamp(6),
+    lesson_id bigint,
+    created_at timestamp(6) not null,
+    ind integer not null,
+    primary key (person_id, ind)
+);
+
+create table reg_user (
+    id bigint not null,
+    created_at timestamp(6) not null,
+    disabled boolean,
+    email varchar(100) not null unique,
+    name varchar(50) not null,
+    roles jsonb,
+    surname varchar(50) not null,
+    telPhone varchar(20) not null,
+    verified boolean,
+    primary key (id)
+);
+
+create table reg_user_known_person (
+    user_id bigint not null,
+    name varchar(50) not null,
+    relation varchar(10) not null,
+    surname varchar(50) not null,
+    ind integer not null,
+    primary key (user_id, ind)
+);
+
+create index FKrude4o9qg6b1kehdii9sqxf2x on reg_attendance (lesson_id);
+alter table if exists reg_attendance 
+   add constraint FKrude4o9qg6b1kehdii9sqxf2x 
+   foreign key (lesson_id) 
+   references reg_lesson;
+
+create index FKeh9qwfyea5pwv0y0spc8j6rw9 on reg_attendance (person_id);
+alter table if exists reg_attendance 
+   add constraint FKeh9qwfyea5pwv0y0spc8j6rw9 
+   foreign key (person_id) 
+   references reg_person;
+
+create index FKdx49qbmdqvrfhbr4k70u4ohq9 on reg_attendance_trainer (lesson_id);
+alter table if exists reg_attendance_trainer 
+   add constraint FKdx49qbmdqvrfhbr4k70u4ohq9 
+   foreign key (lesson_id) 
+   references reg_lesson;
+
+create index FKruj6st46w25y23h6x0173l4jr on reg_attendance_trainer (person_id);
+alter table if exists reg_attendance_trainer 
+   add constraint FKruj6st46w25y23h6x0173l4jr 
+   foreign key (person_id) 
+   references reg_person;
+
+create index FKekjbpo9eko6r6u4mmr0qd89vb on reg_event (period_id);
+alter table if exists reg_event 
+   add constraint FKekjbpo9eko6r6u4mmr0qd89vb 
+   foreign key (period_id) 
+   references reg_period;
+
+create index FKpor1e24qsfson7f3fxwfoypfe on reg_event (place_id);
+alter table if exists reg_event 
+   add constraint FKpor1e24qsfson7f3fxwfoypfe 
+   foreign key (place_id) 
+   references reg_place;
+
+create index FKn5r2rv274w1jmhrfopttnlmgh on reg_event_condition (event_id);
+alter table if exists reg_event_condition 
+   add constraint FKn5r2rv274w1jmhrfopttnlmgh 
+   foreign key (event_id) 
+   references reg_event;
 
 create index FK3amist06sen6r7d9m164epbr4 on reg_event_term (event_id);
 alter table if exists reg_event_term 
@@ -127,11 +294,23 @@ alter table if exists reg_known_person
    foreign key (registration_id) 
    references reg_registration;
 
-create index FKmfsff7uq7d9ftjk360nuspw18 on reg_person (registration_id);
+create index FK7badtl7mhctw5h9v7wkccuc1x on reg_lesson (event_term_id);
+alter table if exists reg_lesson 
+   add constraint FK7badtl7mhctw5h9v7wkccuc1x 
+   foreign key (event_term_id) 
+   references reg_event_term;
+
+create index FKm2k40vsbt2s507jv6f12fom1k on reg_lesson (place_id);
+alter table if exists reg_lesson 
+   add constraint FKm2k40vsbt2s507jv6f12fom1k 
+   foreign key (place_id) 
+   references reg_place;
+
+create index FKbjayrwgy5k5b0sgo32v5k9bvq on reg_person (user_id);
 alter table if exists reg_person 
-   add constraint FKmfsff7uq7d9ftjk360nuspw18 
-   foreign key (registration_id) 
-   references reg_registration;
+   add constraint FKbjayrwgy5k5b0sgo32v5k9bvq 
+   foreign key (user_id) 
+   references reg_user;
 
 create index FKli5l6y8g2i9ww46ctuewlhw0h on reg_registration (event_term_id);
 alter table if exists reg_registration 
@@ -139,8 +318,26 @@ alter table if exists reg_registration
    foreign key (event_term_id) 
    references reg_event_term;
 
-create index FKlm2mjdcnm4qqn1dwm715yvwmp on reg_registration (payment_id);
+create index FKnjnpvb8l9ocogq7bkibd61yt9 on reg_registration (person_id);
 alter table if exists reg_registration 
-   add constraint FKlm2mjdcnm4qqn1dwm715yvwmp 
-   foreign key (payment_id) 
-   references reg_payment;
+   add constraint FKnjnpvb8l9ocogq7bkibd61yt9 
+   foreign key (person_id) 
+   references reg_person;
+
+create index FK31csvudebfdubxfcph0os1jk3 on reg_substitute_lesson (lesson_id);
+alter table if exists reg_substitute_lesson 
+   add constraint FK31csvudebfdubxfcph0os1jk3 
+   foreign key (lesson_id) 
+   references reg_lesson;
+
+create index FK15avd1i3eo3x9byi7og8jf8ki on reg_substitute_lesson (person_id);
+alter table if exists reg_substitute_lesson 
+   add constraint FK15avd1i3eo3x9byi7og8jf8ki 
+   foreign key (person_id) 
+   references reg_person;
+
+create index FKdfbkmq04awfqi59v3ogpeewow on reg_user_known_person (user_id);
+alter table if exists reg_user_known_person 
+   add constraint FKdfbkmq04awfqi59v3ogpeewow 
+   foreign key (user_id) 
+   references reg_user;
