9b5c3b9f585eb61c1324a723e24014bc14ed20ae
milossilhar <migmig095@gmail.com>
2025-06-20 14:35:27 +0200

    aux: removed unused imports

--- src/main/java/sk/leziemevpezinku/spring/model/Person.java
+++ src/main/java/sk/leziemevpezinku/spring/model/Person.java
@@ -2,11 +2,9 @@ package sk.leziemevpezinku.spring.model;
 
 import jakarta.persistence.*;
 import lombok.*;
-import sk.leziemevpezinku.spring.util.StringUtils;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Objects;
 
 @Data
 @Builder
--- src/main/java/sk/leziemevpezinku/spring/repo/EventTermRepository.java
+++ src/main/java/sk/leziemevpezinku/spring/repo/EventTermRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 import sk.leziemevpezinku.spring.model.EventTerm;
 
