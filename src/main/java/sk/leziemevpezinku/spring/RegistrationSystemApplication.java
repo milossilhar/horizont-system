1e5d5a2dfba2b86f1abd631775def705da7f28f5
milossilhar <migmig095@gmail.com>
2025-07-03 11:41:03 +0200

    chg: added @OpenAPIDefinition for generated openapi.yaml

--- src/main/java/sk/leziemevpezinku/spring/RegistrationSystemApplication.java
+++ src/main/java/sk/leziemevpezinku/spring/RegistrationSystemApplication.java
@@ -1,5 +1,8 @@
 package sk.leziemevpezinku.spring;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,6 +11,15 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableScheduling
 @SpringBootApplication
 @EnableTransactionManagement
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Registration System API",
+                version = "1.0.0"
+        ),
+        servers = {
+                @Server(url = "http://localhost:8080", description = "Development server")
+        }
+)
 public class RegistrationSystemApplication {
 
     /**
