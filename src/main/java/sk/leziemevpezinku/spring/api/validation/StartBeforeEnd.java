5599865d309cc1922f33ce2062cd2e98bfecba8f
milossilhar <migmig095@gmail.com>
2025-07-08 12:30:03 +0200

    chg: using StartBeforeEnd multiple times

--- src/main/java/sk/leziemevpezinku/spring/api/validation/StartBeforeEnd.java
+++ src/main/java/sk/leziemevpezinku/spring/api/validation/StartBeforeEnd.java
@@ -4,14 +4,14 @@ import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 import sk.leziemevpezinku.spring.api.validation.impl.StartBeforeEndValidator;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
-import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Target(TYPE)
 @Retention(RetentionPolicy.RUNTIME)
+@Repeatable(StartBeforeEnd.List.class)
 @Constraint(validatedBy = StartBeforeEndValidator.class)
 public @interface StartBeforeEnd {
     String message() default "Start must be before end.";
@@ -20,4 +20,11 @@ public @interface StartBeforeEnd {
 
     String startProperty();
     String endProperty();
+
+
+    @Target(TYPE)
+    @Retention(RUNTIME)
+    @interface List {
+        StartBeforeEnd[] value();
+    }
 }
