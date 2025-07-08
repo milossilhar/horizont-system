a2c1f23752ccf73438ebc77100e733cc518c5b4f
milossilhar <migmig095@gmail.com>
2025-07-08 23:10:33 +0200

    aux: removed some EnumerationValues

--- src/main/java/sk/leziemevpezinku/spring/api/enumeration/EnumerationValues.java
+++ src/main/java/sk/leziemevpezinku/spring/api/enumeration/EnumerationValues.java
@@ -2,7 +2,5 @@ package sk.leziemevpezinku.spring.api.enumeration;
 
 public interface EnumerationValues {
 
-    interface REG_EVENT_DISCOUNT_TYPE {
-        String LETO_TABOR_25 = "25TABOR";
-    }
+
 }
